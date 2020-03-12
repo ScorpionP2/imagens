1bf4eb5a9f98243b410314dc549c0c322f7b8c2e
ScorpionP2 <icaronascimento85@gmail.com>
2020-03-12 10:33:17 -0300

    text view tipo peso

--- app/src/main/java/com/example/app_imc/CalcularIMC.java
+++ app/src/main/java/com/example/app_imc/CalcularIMC.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CalcularIMC extends AppCompatActivity {
 EditText txtPeso,txtAltura;
 ImageView imgResultadoImc;
+TextView txtRIMC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +21,8 @@ ImageView imgResultadoImc;
 
         txtAltura = (EditText)findViewById(R.id.txtAltura);
         txtPeso = (EditText)findViewById(R.id.txtPeso);
+        txtRIMC = (TextView) findViewById(R.id.rimc);
+
         imgResultadoImc = (ImageView)findViewById(R.id.imgResultadoImc);
 
     }
@@ -32,21 +36,27 @@ ImageView imgResultadoImc;
                 imc=(peso/(altura*altura));
                 if(imc<=18.5){
                     imgResultadoImc.setImageResource(R.drawable.abaixopeso);
+                    txtRIMC.setText("Abaixo do peso");
                 }
                 else if((imc>=18.6)&&(imc<=24.9)){
                     imgResultadoImc.setImageResource(R.drawable.normal);
+                    txtRIMC.setText("Normal");
                 }
                 else if((imc>=25.0)&&(imc<=29.9)){
                     imgResultadoImc.setImageResource(R.drawable.sobrepeso);
+                    txtRIMC.setText("sobrepeso");
                 }
                 else if((imc>=30.0)&&(imc<=34.9)){
                     imgResultadoImc.setImageResource(R.drawable.obesidade1);
+                    txtRIMC.setText("Obesidade 1");
                 }
                 else if((imc>=35.0)&&(imc<=39.9)){
                     imgResultadoImc.setImageResource(R.drawable.obesidade2);
+                    txtRIMC.setText("Obesidade 2");
                 }
                 else if(imc>=40.0){
                     imgResultadoImc.setImageResource(R.drawable.obesidade3);
+                    txtRIMC.setText("Obesidade 3");
                 }
             }
             else{
